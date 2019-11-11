871e59abfd6eb3c1ce42775f74a783857eaf6fce
Federico Capece <fedechico1@live.it>
2019-11-11 03:26:51 +0100

    removed log, now main name is like

--- q2a.sh
+++ q2a.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-cd ~/Uniroma3_Feed
+cd ~/Q2A_Pi
 
 #update to git last version
 git fetch --all
 git reset --hard origin/master
 
 #set every file executable
-chmod -R 777 ~/Uniroma3_Feed
+chmod -R 777 ~/Q2A_Pi
 
 #run script
-date +"%d/%m/%Y %H:%M" > log.txt
-./main.py >> log.txt
+date +"%d/%m/%Y %H:%M"
+./likes.py

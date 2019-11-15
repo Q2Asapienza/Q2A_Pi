4b32a84aec7e6d56d3bf848ebbfa1610e9045854
Federico Capece <fedechico1@live.it>
2019-11-15 23:18:57 +0100

    now q2a works with dictionaries and not objects

--- __init__.py
+++ __init__.py
@@ -5,29 +5,139 @@ import cssselect
 from lxml import html
 from typing import List
 
-class Q2A:
-    BASE_URL = "https://q2a.di.uniroma1.it/"
-    QUESTION_PAGE = "https://q2a.di.uniroma1.it/questions/"
-    LOGIN_URL = "https://q2a.di.uniroma1.it/login?to="
-    USER_URL = "https://q2a.di.uniroma1.it/user"
-    ACTIVITY_PAGE = "https://q2a.di.uniroma1.it/activity"
+#urls
+URL_BASE        = "https://q2a.di.uniroma1.it/"
+URL_QUESTIONS   = "https://q2a.di.uniroma1.it/questions/"
+URL_LOGIN       = "https://q2a.di.uniroma1.it/login?to="
+URL_USER        = "https://q2a.di.uniroma1.it/user"
+URL_USERS       = "https://q2a.di.uniroma1.it/users"
+URL_ACTIVITIES  = "https://q2a.di.uniroma1.it/activity/"
 
-    def __init__(self, username:str, password:str, category = "fondamenti-di-programmazione-19-20"):
+#KEYS FOR DICTIONARIES
+#types
+KEY_TYPE        = 'types'
+KEY_QUESTIONS   = 'questions'
+KEY_ANSWERS     = 'answers'
+KEY_COMMENTS    = 'comments'
+
+#general data
+KEY_ID          = 'id'
+KEY_TEXT        = 'text'
+KEY_PARENT      = 'parent'
+#edits
+KEY_CREATED     = 'created'
+KEY_LAST_EDIT   = 'last_edit'
+
+KEY_TIMESTAMP   = 'timestamp'
+KEY_USER        = 'user'
+
+#question data
+KEY_TITLE       = 'title'
+#answer data
+KEY_BEST        = 'best'
+#like data
+KEY_VOTED       = 'voted'
+
+def Q2ADictToSerializable(q2a_dict:dict):
+    q2a_dict = q2a_dict.copy()
+    for key,value in q2a_dict.items():
+        if key == KEY_PARENT and isinstance(value,dict):
+            #removing circular reference
+            q2a_dict[key] = value[KEY_ID]
+        elif isinstance(value,dict):
+            q2a_dict[key] = Q2AToSerializable(value)
+    
+    return q2a_dict
+
+class Q2A:
+    LIKE_HEADERS = {
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest",
+        "origin": URL_BASE,
+    }
+    #region HIDDEN METHODS
+    def __init__(self, username:str = None, password:str = None, session_file:str = None, category = "fondamenti-di-programmazione-19-20"):
         """
         """
+        #initializing class attributes
+        self.cache  = {}
+        self.category = category
         self.username = username
         self.password = password
-        self.category = category
+        self.session = None
 
-        self.session = requests.session()
-        self.session.headers.update({"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"})
+        #PREPARING SESSION
+        #if there is a session file i load it
+        if session_file != None:
+            self.sessionLoad(session_file)
+        
+        #if i don't have a session loaded
+        if self.session == None:
+            #if i have a password i create it, otherwise i start a blank session
+            if password != None:
+                self.sessionCreate()
+            else:
+                self.session = requests.session()
+                self.session.headers.update({"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"})
 
-    #region utility
-    def getHTMLFromURL(self, url:str) -> html.HtmlElement:
-        return html.fromstring(self.session.get(url).text)
+    #region INNER UTILITIES
+    def __getHTMLFromURL(self, url:str, cache=True) -> html.HtmlElement:
+        if not cache or url not in self.cache:
+            self.cache[url] = html.fromstring(self.session.get(url).text)
+        return self.cache[url]
 
-    def getCode(self, tree:html.HtmlElement) -> str:
+    @staticmethod
+    def __getCode(tree:html.HtmlElement) -> str:
         return tree.cssselect('input[name="code"]')[0].attrib['value']
+
+    @staticmethod
+    def __userID(userUrl:str) -> str:
+        return userUrl.split('/')[-1]
+
+    @staticmethod
+    def __getEdit(tree:html.HtmlElement,index:int) -> dict:
+        edit_timestamp = tree.cssselect('.updated .value-title')[index].attrib['title']
+        edit_who = Q2A.__userID(tree.cssselect('.author a')[index].attrib['href'])
+        return {KEY_USER: edit_who, KEY_TIMESTAMP:edit_timestamp}
+
+    @staticmethod
+    def __firstEdit(tree:html.HtmlElement) -> dict:
+        return Q2A.__getEdit(tree,0)
+
+    @staticmethod
+    def __lastEdit(tree:html.HtmlElement) -> dict:
+        return Q2A.__getEdit(tree,-1)
+
+    def __questionsFromURL(self,url:str) ->dict:
+        tree = self.__getHTMLFromURL(url)
+        questions = {}
+        pageQuestions = tree.cssselect('.qa-part-q-list .qa-q-list-item')
+        for questionDiv in pageQuestions:
+            question = {KEY_TYPE:KEY_QUESTIONS}
+            #getting out data from question
+            
+            question[KEY_ID]            = questionDiv.attrib['id'][1:]
+            question[KEY_TITLE]         = questionDiv.cssselect(".qa-q-item-title span")[0].text
+            #getting last edit from what, when, who, not really reliable as it depends on if you use questions or activities
+            #who = questionDiv.cssselect('.qa-user-link')[0].attrib['href']
+            #question[KEY_LAST_EDI T] = {
+            #    'what':questionDiv.cssselect('.qa-q-item-what')[0].text,
+            #    'when':questionDiv.cssselect('.qa-q-item-when-data')[0].text,
+            #    'who':who[who.rfind('/')]
+            #}
+
+            #navigating to question to get inner data
+            questionInnerDiv = self.__getHTMLFromURL(URL_BASE+question[KEY_ID]).cssselect('div.question')[0]
+            question[KEY_CREATED]       = Q2A.__firstEdit(questionInnerDiv)
+            question[KEY_LAST_EDIT]     = Q2A.__lastEdit(questionInnerDiv)
+            question[KEY_TEXT]          = questionInnerDiv.cssselect(".entry-content")[0].text_content()
+
+            #appending question to questions
+            questions[question[KEY_ID]] = question
+
+        return questions
+    #endregion
     #endregion
 
     #region SESSION management
@@ -38,13 +148,10 @@ class Q2A:
 
         Params:
         filename(str): the path of the file in wich the session will be written
-
-        Return:
-        True if the session was loaded and still valid, False otherwise
         """
 
         # Get csrf token
-        page = self.session.get(Q2A.BASE_URL).text
+        page = self.session.get(URL_BASE).text
         tree = html.fromstring(page)
         self
 
@@ -52,12 +159,12 @@ class Q2A:
             "emailhandle": self.username, 
             "password": self.password,
             "remember": '1',
-            "code": self.getCode(tree),
+            "code": Q2A.__getCode(tree),
             "dologin": "Login",
         }
 
         # Perform login
-        self.session.post(Q2A.LOGIN_URL, data = loginPOSTBody)
+        self.session.post(URL_LOGIN, data = loginPOSTBody)
 
         if(not self.logged_in()):
             raise Exception("CAN'T LOGIN")
@@ -99,26 +206,35 @@ class Q2A:
 
     #region PROFILE INFO
     def logged_in(self):
-        return self.username in self.session.get(self.USER_URL).url
+        return self.username in self.session.get(URL_USER).url
 
     def profileInfo(self):
         pass
     #endregion
 
     #region Utility for user
-    def getAllQuestions(self, category:str = None) -> list:
+    def getQuestions(self, category:str = None) -> dict:
+        """
+        Get questions from all the pages.
+
+        Params:
+        category(str): The id of the category
+
+        Return:
+        A list containing the Questions found
+        """
         page = 1
-        questions = []
+        questions = {}
         while(True):
-            added = self.getQuestions(category=category,page=page)
+            added = self.getQuestionsFromPage(category=category,page=page)
             #going to next page
             if(len(added) == 0):
                 break
-            questions.extend(added)
+            questions.update(added)
             page += 1
         return questions
-
-    def getQuestions(self, page:int = 1, category:str = None) -> list:
+    
+    def getQuestionsFromPage(self, page:int = 1, category:str = None) -> dict:
         """
         Get questions from a page.
 
@@ -132,136 +248,136 @@ class Q2A:
         if(category == None):
             category = self.category
 
-        return self.__questionsFromURL(self.QUESTION_PAGE+category +"?start=" +str((page-1)*20))
-        #loading page
+        return self.__questionsFromURL(URL_QUESTIONS+category +"?start=" +str((page-1)*20))
+    
+    def getQuestionsFromActivities(self, category:str = None) -> dict:
+        """
+        Get questions from activities.
 
-    def getQuestionsFromActivities(self, category:str = None):
+        Params:
+        page(int): The number of the page
+        category(str): The id of the category
+
+        Return:
+        A list containing the Questions found
+        """
         if(category == None):
             category = self.category
-        return self.__questionsFromURL(self.ACTIVITY_PAGE + category)
-        
-        
+        return self.__questionsFromURL(URL_ACTIVITIES + category)
+    
+    def getAnswersFromQuestions(self,questions:dict, update=True) -> dict:
+        answers = {}
+        for question in questions.values():
+            answers.update(self.getAnswersFromQuestion(question), update)
+        return answers
 
-    def __questionsFromURL(self,url:str) ->list:
-        tree = self.getHTMLFromURL(url)
-        questions = []
-        currentQuestions = tree.cssselect('.qa-part-q-list .qa-q-list-item')
-        for question in currentQuestions:
-            question_id = question.attrib['id'][1:]
-            question_title = question.cssselect(".qa-q-item-title span")[0].text
-            who = question.cssselect('.qa-user-link')[0].attrib["href"]
-            lastEdit = {
-                "what":question.cssselect('.qa-q-item-what')[0].text,
-                "when":question.cssselect('.qa-q-item-when-data')[0].text,
-                'who':User(who[who.rfind('/')])
-                }
-            if(question_id not in questions):
-                questions.append(Question(question_id,self,title=question_title, lastEdit=lastEdit))
-        return questions
+    def getAnswersFromQuestion(self,question:dict, update=True) -> dict:
+        pageAnswers = self.__getHTMLFromURL(URL_BASE + question[KEY_ID]).cssselect("div.answer")
+        answers = {}
+        for answerDiv in pageAnswers:
+            answer = {KEY_TYPE:KEY_ANSWERS}
 
-    def getLikes(self, questions:list ,refresh:bool=False) -> list:
-        likes = []
-        question:Question
-        for question in questions:
-            if(refresh):
-                question.get()
-                likes.extend(question.likes)
-        return likes
-    #endregion
+            #getting data from answer
+            answer[KEY_ID]            = answerDiv.attrib['id'][1:]
+            
+            #this is necessary otherwise it will get the last edit from the last comment inside the answer
+            editDiv = answerDiv.cssselect(".qa-a-item-wrapper")[0]
+            answer[KEY_CREATED]    = Q2A.__firstEdit(editDiv)
+            answer[KEY_LAST_EDIT]     = Q2A.__lastEdit(editDiv)
 
-class Like:
-    LIKE_HEADERS = {
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "x-requested-with": "XMLHttpRequest",
-        "origin": "https://q2a.di.uniroma1.it",
-    }
+            answer[KEY_TEXT]          = answerDiv.cssselect(".entry-content")[0].text_content()
 
-    def __init__ (self, id:str, question, voted:bool):
-        self.id = id
-        self.question:Question = question
-        self.headers = Like.LIKE_HEADERS.copy()
-        self.question = question
-        self.headers['referer'] = self.question.id
-        self.voted = voted
+            answer[KEY_PARENT]        = question
+            
+            if(len(answerDiv.cssselect('.qa-a-selected')) != 0):
+                question [KEY_BEST] =  answer[KEY_ID] 
 
-    def vote(self, upVote:bool = True):
+            #appending question to questions
+            answers[answer[KEY_ID] ] = answer
+        if(update):
+            question[KEY_ANSWERS] = answers
+        return answers
+
+    def getCommentsFromAnswers(self,answer:dict, update=True):
+        comments = {}
+        for question in questions.values():
+            comments.update(self.getAnswersFromQuestion(question),update)
+        return comments
+
+    def getCommentsFromAnswer(self, answer:dict, update=True):
+        answerComments = self.__getHTMLFromURL(URL_BASE + answer[KEY_PARENT][KEY_ID]).cssselect(f'#a{answer[KEY_ID]} .comment')
+        comments = {}
+        for commentDiv in answerComments:
+            comment = {KEY_TYPE:KEY_ANSWERS}
+
+            #getting data from answer
+            comment[KEY_ID]            = commentDiv.attrib['id'][1:]
+            comment[KEY_CREATED]    = Q2A.__firstEdit(commentDiv)
+            comment[KEY_LAST_EDIT]     = Q2A.__lastEdit(commentDiv)
+            comment[KEY_TEXT]          = commentDiv.cssselect(".entry-content")[0].text_content()
+
+            comment[KEY_PARENT]        = answer
+
+            #appending question to questions
+            comments[comment[KEY_ID] ] = comment
+        if(update):
+            answer[KEY_COMMENTS] = comments
+        return comments
+
+    def sendVote(self, like:dict, upVote:bool = True):
         #navigate to Question otherwise site goes MAD (and meanwhile i update code)
-        tree = self.question.getHTML()
-        form = tree.xpath("//div[@id='voting_"+self.id+"']/ancestor::form")[0]
+        question_id = like['question']['id']
+        like_id = like['id']
+
+        tree = self.__getHTMLFromURL(URL_BASE+question_id)
+        form = tree.xpath("//div[@id='voting_"+like_id+"']/ancestor::form")[0]
+
+        headers = self.LIKE_HEADERS.copy()
+        headers['referer'] = question_id
 
         postData = {
-            "postid": self.id,
+            "postid": like_id,
             "vote": int(upVote),
-            "code": self.question.q2a.getCode(form),
+            "code": Q2A.__getCode(form),
             "qa": "ajax",
             "qa_operation": "vote",
             "qa_root": "./",
-            "qa_request": self.question.id,
+            "qa_request": question_id,
         }
 
-        result = self.question.q2a.session.post(Like.LIKE_HEADERS["origin"], headers=self.headers, data = postData).text.split('\n')
+
+        result = self.session.post(URL_BASE, headers=headers, data = postData).text.split('\n')
         if result[1] == '1':
             return True
         
         return result[2]
-
-class Question:
-    __dict = {}
-    __id:str
-
-    @property
-    def id(self) -> str:
-        return self.__id
-    likes:list
-    lastEdit:dict
-    title:str
-    preview:str
-
-    def __init__(self,id,q2a,**args):
-            self.q2a:Q2A = q2a
-            for key,value in args.items():
-                self.__setattr__(key,value)
-
-            if id in Question.__dict:
-                self = Question.__dict[id]
-            else:
-                self.__id = id
-                #self.update()#TODO: ABILITA
-                Question.__dict[id] = self
-    
-    def getHTML(self):
-        return self.q2a.getHTMLFromURL(Q2A.BASE_URL+self.__id, self.q2a.session)
-
-    def get(self):
-        self.likes = []
-        tree = self.q2a.getHTMLFromURL(Q2A.BASE_URL+self.__id, self.q2a.session)
-        for like in tree.cssselect(".qa-voting"):
-            name = like.cssselect(".qa-vote-one-button")[0].get("name")
-            voted = name if name == None else (name.split("_")[2] == '0')
-            self.likes.append(Like(like.attrib["id"].split("_")[1],self, voted))
-        return self.likes
-    
-    #region compare utility
-    def __hash__(self):
-        return id
-
-    def __eq__(self, other):
-        if isinstance(other, Question):
-            return self.id == other.id
-        elif isinstance(other,int):
-            return self.id == other
-    
-    def __ne__(self, other):
-        result = self.__eq__(other)
-        return result if result is NotImplemented else not result 
     #endregion
 
-class User:
-    __id:str
-    @property
-    def id(self):
-        return self.__id
+
+    def getLikes(self,**questions)-> list:
+        likes = []
+        for question in questions:
+            tree = self.__getHTMLFromURL(URL_BASE+question[KEY_ID])
+            for like in tree.cssselect(".qa-voting"):
+                name = like.cssselect(".qa-vote-one-button")[0].get("name")
+                voted = name if name == None else (name.split("_")[2] == '0')
+                likes.append({KEY_ID:like.attrib["id"].split("_")[1], KEY_VOTED:voted})
+        return likes
+
+if __name__ == "__main__":
+    import pprint,json
+    pp = pprint.PrettyPrinter()
     
-    def __init__(self,id):
-        self.__id = id
+
+    q2a = Q2A()
+    questions = q2a.getQuestionsFromActivities()
+    question1 = questions[list(questions.keys())[0]]
+    
+    answers = q2a.getAnswersFromQuestion(question1)
+    answer1 = answers[list(answers.keys())[0]]
+
+    comments = q2a.getCommentsFromAnswer(answer1)
+    comment1 = comments[list(comments.keys())[0]]
+        
+    print(json.dumps(questions, indent=4, sort_keys=True))
+    #print(pprint.pformat(questions))
